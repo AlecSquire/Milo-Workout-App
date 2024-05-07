f2f2b796c2d2dd390e749c879ff203cb2793d0ed
Alexander George Squire <alecsquire@gmail.com>
2024-05-07 09:58:29 +0100

    modal added and styled, start workout creates dyanmic router.push (yet to dyanmically set the url to the workout name)

--- app/routines/UserTemplates.tsx
+++ app/routines/UserTemplates.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState, FormEvent } from "react";
 import { Card } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
 import {
   Carousel,
   CarouselContent,
@@ -42,8 +43,10 @@ interface Routine {
 }
 
 const UserTemplates: React.FC = ({ userTemplates, setUserTemplates }) => {
+  const router = useRouter();
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [routineToDelete, setRoutineToDelete] = useState<Routine | null>(null);
+  const [modalTemplate, setModalTemplate] = useState(false);
 
   useEffect(() => {
     const q = query(collection(db, "userRoutines"));
@@ -80,16 +83,17 @@ const UserTemplates: React.FC = ({ userTemplates, setUserTemplates }) => {
         <div
           key={index}
           className="flex items-center justify-between w-full p-4 border border-gray-300 rounded cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-md w-100%"
+          onClick={() => setModalTemplate(true)} // Open modal when clicking on the routine item
         >
           <div>
             <h2 className="text-lg font-bold mb-2">{routine.item}</h2>
             <p className="text-sm text-gray-700 mb-4">{routine.description}</p>
-
             <div className="flex items-center">
               <DropDown id={routine.id} />
               <Trash
                 className="ml-2 h-4 w-4 cursor-pointer"
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   setRoutineToDelete(routine);
                   setDeleteDialogOpen(true);
                 }}
@@ -98,6 +102,41 @@ const UserTemplates: React.FC = ({ userTemplates, setUserTemplates }) => {
           </div>
         </div>
       ))}
+      {/* Modal Preview of workout template */}
+      {modalTemplate && (
+        <AlertDialog
+          open={modalTemplate} // Set open to modalTemplate
+          onClose={() => setModalTemplate(false)}
+        >
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Workout Name</AlertDialogTitle>
+              <AlertDialogDescription>
+                {" "}
+                <ul>
+                  <li>Exercise 1</li>
+                  <li>Exercise 2</li>
+                  <li>Exercise 3</li>
+                  <li>Exercise 4</li>
+                </ul>
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel onClick={() => setModalTemplate(false)}>
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  setModalTemplate(false); // Close the modal
+                  router.push("routines/[RoutineID]"); // Navigate to the new route
+                }}
+              >
+                Start Workout
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
 
       {/* Alert Dialog */}
       <AlertDialog
